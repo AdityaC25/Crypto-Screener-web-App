f6f234bd8cfd977351c5ba3f50dd2c35ff7d9019
Aditya Choudhary <adityachoudhary252002@gmail.com>
2024-03-04 00:34:06 +0530

    complete my final code

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className='bg-white text-black'>
+        <p className='text-red-700'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {
   createBrowserRouter,
@@ -11,28 +11,49 @@ import Home from './pages/Home';
 import Crypto from './pages/Crypto';
 import Trending from './pages/Trending';
 import Saved from './pages/Saved';
-
+import CryptoDetails from './components/CryptoDetails';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home/>,
+    element: <Home />,
     children:[
       {
-      path:"/",
-      element:<Crypto/>
-    },
-    {
-      path:"/trending",
-      element:<Trending/>
-    },
-    {
-      path:"/saved",
-      element:<Saved/>
-    }
-  ]
+        path:"/",
+        element: <Crypto />,
+        children: [
+          {
+            path:":coinId",
+            element: <CryptoDetails />
+          }
+        ]
+      },
+      {
+        path:"/trending",
+        element: <Trending />,
+        children: [
+          {
+            path:":coinId",
+            element: <CryptoDetails />
+          }
+        ]
+      },
+      {
+        path:"/saved",
+        element: <Saved />,
+        children: [
+          {
+            path:":coinId",
+            element: <CryptoDetails />
+          }
+        ]
+      }
+    ]
+
+
   },
 ]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
